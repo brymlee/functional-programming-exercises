93d48495e9aee71cc50e5900284707a2d5b8ab88
Evan Kimberlin <brymlee1911@gmail.com>
2018-03-19 14:27:33 -0500

    Fixing parent project

--- filtering-exercise/src/test/java/brymlee/FilteringServiceTest.java
+++ filtering-exercise/src/test/java/brymlee/FilteringServiceTest.java
@@ -7,7 +7,7 @@ import cucumber.api.CucumberOptions;
 
 @RunWith(Cucumber.class)
 @CucumberOptions(features = {"src/test/"})
-public class FilterServiceTest{
+public class FilteringServiceTest{
 
 
 }
